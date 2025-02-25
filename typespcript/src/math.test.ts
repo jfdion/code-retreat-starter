16d9330c8a7991248617e98509d0b5a73ba1e3df
Jean-Francois Dion <jfdion@csfoy.ca>
2025-02-25 09:38:55 -0500

    Update math.test.ts

--- typespcript/src/math.test.ts
+++ typespcript/src/math.test.ts
@@ -5,8 +5,8 @@ test(`can add two numbers`, () => {
     const result: number = sum(1, 2);
 
     expect(result).toBe(3);
-})
+});
 
 test(`test that fails`, () => {
     expect(false).toBe(true);
-})
+});
